24f6360c705b6a12968db21cbad88263dbac5886
Nicolás Jiménez Olmedo <nicjimolm@us.es>
2023-11-08 22:25:49 +0100

    que sea lo que dios quiera

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,1 +1,5 @@
-Created by Nicolás Jiménez Olmedo
+
+<a style="font-family:'Times New Roman', Times, serif; font-size:30px" href="https://www.youtube.com/watch?v=dXzA7ctduzs">· Vídeo Explicativo</a>
+    <br>
+
+
